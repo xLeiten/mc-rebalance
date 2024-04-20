4bb897d08d297a63695f0d0a9d73a639af0e3ab1
xLeiten <oflisetrix@gmail.com>
2024-04-20 20:40:44 +0700

    Change netherite armor points

--- src/main/java/me/xleiten/rebalance/core/mixins/world/items/armor_rebalance/MixinArmorMaterials.java
+++ src/main/java/me/xleiten/rebalance/core/mixins/world/items/armor_rebalance/MixinArmorMaterials.java
@@ -1,14 +1,16 @@
 package me.xleiten.rebalance.core.mixins.world.items.armor_rebalance;
 
 import net.minecraft.item.ArmorMaterials;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-import static me.xleiten.rebalance.core.game.item.ArmorBuffsOptions.*;
+import static me.xleiten.rebalance.core.game.item.armor.ArmorBuffsOptions.*;
 
+@Debug(export = true)
 @Mixin(ArmorMaterials.class)
 public abstract class MixinArmorMaterials
 {
@@ -33,7 +35,7 @@ public abstract class MixinArmorMaterials
                     ordinal = 2
             )
     )
-    private static void changeIronArmorToughness(Args args) {
+    private static void changeIronArmorProperties(Args args) {
         args.set(7, IRON_TOUGHNESS.getValue());
         args.set(8, IRON_KNOCKBACK_RESISTANCE.getValue());
     }
@@ -46,7 +48,7 @@ public abstract class MixinArmorMaterials
                     ordinal = 4
             )
     )
-    private static void changeDiamondArmorToughness(Args args) {
+    private static void changeDiamondArmorProperties(Args args) {
         args.set(7, DIAMOND_TOUGHNESS.getValue());
         args.set(8, DIAMOND_KNOCKBACK_RESISTANCE.getValue());
     }
@@ -64,4 +66,53 @@ public abstract class MixinArmorMaterials
         return NETHERITE_TOUGHNESS.getValue();
     }
 
+
+    // Netherite armor points changes
+    @ModifyArgs(
+            method = "method_48406",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/EnumMap;put(Ljava/lang/Enum;Ljava/lang/Object;)Ljava/lang/Object;",
+                    ordinal = 0
+            )
+    )
+    private static void changeNetheriteArmorPointsForBoots(Args args) {
+        args.set(1, 4);
+    }
+
+    @ModifyArgs(
+            method = "method_48406",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/EnumMap;put(Ljava/lang/Enum;Ljava/lang/Object;)Ljava/lang/Object;",
+                    ordinal = 1
+            )
+    )
+    private static void changeNetheriteArmorPointsForLeggings(Args args) {
+        args.set(1, 7);
+    }
+
+    @ModifyArgs(
+            method = "method_48406",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/EnumMap;put(Ljava/lang/Enum;Ljava/lang/Object;)Ljava/lang/Object;",
+                    ordinal = 2
+            )
+    )
+    private static void changeNetheriteArmorPointsForChestplate(Args args) {
+        args.set(1, 10);
+    }
+
+    @ModifyArgs(
+            method = "method_48406",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/EnumMap;put(Ljava/lang/Enum;Ljava/lang/Object;)Ljava/lang/Object;",
+                    ordinal = 3
+            )
+    )
+    private static void changeNetheriteArmorPointsForHelmet(Args args) {
+        args.set(1, 4);
+    }
 }
